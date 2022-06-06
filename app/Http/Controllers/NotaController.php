d3a761ac3aa60adaee74335ebb994802d37071be
Taiane Melo <taianemedeiro16@gmail.com>
2022-06-06 14:22:12 -0400

    testXML

--- app/Http/Controllers/NotaController.php
+++ app/Http/Controllers/NotaController.php
@@ -34,18 +34,38 @@ class NotaController extends Controller
 
     public function store(Request $request) {
 
-    
+        if($request->method() == "POST"){
+            $xml = simplexml_load_file($request->xml);
+
+            $cnpj_nf = $xml->NFe->infNFe->emit->CNPJ->asXML();
+
+            $cnpj_validado = '<CNPJ>09066241000884</CNPJ>';
+
+            $nProt = str_replace(['<nProt>', '</nProt>'], '', $xml->protNFe->infProt->nProt->asXML()); //<nProt></nProt> str_replace
+
+            if($nProt == null || $nProt == ""){
+                return redirect('notas')->with('error', 'Numero de Protocolo não preenchido'); 
+            }
+
+            if($cnpj_nf != $cnpj_validado){
+                return redirect('notas')->with('error', 'CNPJ inválido'); 
+            }
+
+            $numNota = str_replace(['<nNF>', '</nNF>'], '', $xml->NFe->infNFe->ide->nNF->asXML());      
+
+            $horaEmissao = date('d/m/Y', strtotime(str_replace (['<dhEmi>', "</dhEmi>"], '', $xml->NFe->infNFe->ide->dhEmi->asXML())));
+
             $nota = new Nota;
 
-            $nota->xml = $request->xml;
-               
+            $nota->numNota = $numNota;
+
+            $nota->horaEmissao = $horaEmissao;
+                
             $nota->save();
-
-         
-    return redirect('notas')->with('success', 'Nota salva!');
-     
-
-}
+        }
+            
+        return redirect('notas')->with('success', 'Nota salva!');
+    }
 
     /**
      * Método que visualiza o notao.
@@ -70,7 +90,7 @@ class NotaController extends Controller
      */
     public function delete($id) {
 
-        $nota = nota::find($id);
+        $nota = Nota::find($id);
 
         return view('notas.delete', compact('nota'));
 
--- resources/views/notas/create.blade.php
+++ resources/views/notas/create.blade.php
@@ -8,14 +8,14 @@
         <h2 class="fw-bold">
             <ion-icon name="person-add-outline"></ion-icon> Adicionar Nota
         </h2>
-        <form action="{{ route('notas.store') }}" method="POST" id=form enctype="multipart/form-data">
+        <form action="{{ route('notas.store') }}" method="POST" id="form" enctype="multipart/form-data">
             @csrf
         
-                 <div class="form-group">
-                     <label for="xml">Select XML File:</label>
-                     <input type="file" accept=".xml" class="form-control" required id="xml" name="xml">
-                 </div>
-     
+            <div class="form-group">
+                <label for="xml">Select XML File:</label>
+                <input type="file" accept=".xml" class="form-control" id="xml" name="xml" required>
+            </div>
+
             <a class="btn btn-dark" href="{{ route('notas.index') }}">Cancelar</a>
             <input type="submit" class="btn btn-primary" value="Adicionar nota">
         </form>
